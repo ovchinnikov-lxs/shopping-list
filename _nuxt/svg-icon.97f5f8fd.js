2316b53f5a64c61ee466092b651905f58c46ded2
ovchinnikov-lxs <ovchinnikov-lxs@users.noreply.github.com>
2023-10-10 20:46:11 +0000

    deploy: 6062296f948971915295d97e8a028ab7b16175f7

---
+++ _nuxt/svg-icon.97f5f8fd.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./svg-icon.vue.7570b3ef.js";import"./entry.8ebc3f81.js";export{m as default};
---
+++ _nuxt/ui.219a4665.js
@@ -0,0 +1,1 @@
+import"./entry.8ebc3f81.js";const e=""+new URL("ui.28a10183.svg",import.meta.url).href;export{e as default};
