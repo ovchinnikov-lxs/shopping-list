78e4ec8cb292b03c589b761b2bb9dc23b0b67440
ovchinnikov-lxs <ovchinnikov-lxs@users.noreply.github.com>
2024-02-20 23:47:13 +0000

    deploy: 42f0c356482189b39747caedf3ff1662ceb1c352

---
+++ _nuxt/UiButton.mvY7s1Yh.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./UiButton.vue.C1cofFi6.js";import"./entry.DQm97jWm.js";export{m as default};
--- _nuxt/UiButton.sZOQjF2d.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./UiButton.vue.F-oORhtR.js";import"./entry.AF3NS2Oi.js";export{m as default};
